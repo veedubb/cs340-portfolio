9f74003d627aa3ee61cc11a9c507a1da39f5d40c
Tyrell Verley <twverley@gmail.com>
2023-05-18 18:37:53 -0600

    All functionality present other than UPDATE

---
+++ public/js/update_product.js
@@ -0,0 +1,10 @@
+let updateProductForm = document.getElementById('update-product-form');
+
+updateProductForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    let inputProduct = document.getElementById('update-product');
+    let inputNewPrice = document.getElementById('input-newPrice');
+    
+    let 
+})
