a06e94e5abaf107503a73378a94647d83e561ece
megahzord22 <granmega@oregonstate.edu>
2023-05-21 10:39:27 -0400

    fixing one more stupid mistake que sara sara

--- App.js
+++ App.js
@@ -469,14 +469,3 @@ app.put('/update-date', function(req, res, next){
 app.listen(PORT, function() {
     console.log(`Express started on http://localhost:${PORT}; press Ctrl + C to terminate.`)
 });
-
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-  }
